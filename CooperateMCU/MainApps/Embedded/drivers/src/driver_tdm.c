01a5f9ac50b556354955b47a956ccd5c86989bc8
webom2008 <webom2008@hotmail.com>
2015-09-18 10:49:59 +0800

    CO-MCU: add TDMx to project

--- CooperateMCU/MainApps/Embedded/drivers/src/driver_tdm.c
+++ CooperateMCU/MainApps/Embedded/drivers/src/driver_tdm.c
@@ -94,6 +94,22 @@ static void cleanUartRxBuffer(void)
     }while(0 != rLen);
 }
 
+/*****************************************************************************
+ Prototype    : TDMxRead
+ Description  : read data from TDMx
+ Input        : u8 *pCmdVal2Read  
+                u8 *pCmdValLen    
+ Output       : None
+ Return Value :  >= 0: success,  <0: error
+ Calls        : 
+ Called By    : 
+ 
+  History        :
+  1.Date         : 2015/9/18
+    Author       : qiuweibo
+    Modification : Created function
+
+*****************************************************************************/
 int TDMxRead(u8 *pCmdVal2Read, u8 *pCmdValLen)
 {
     TDMxUartPkt pkt;
@@ -102,14 +118,14 @@ int TDMxRead(u8 *pCmdVal2Read, u8 *pCmdValLen)
     u16 checksum = 0;
     char tmp;
 
-    if (1 != Uart4Read(&pkt.header1, 1)) return -1;
-    if (1 != Uart4Read(&pkt.header2, 1)) return -1;
+    if (1 != Uart4Read((char *)&pkt.header1, 1)) return -1;
+    if (1 != Uart4Read((char *)&pkt.header2, 1)) return -1;
 
     if ((TDMX_UART_PKT_HEADER1 == pkt.header1) \
         && (TDMX_UART_PKT_HEADER2 == pkt.header2))
     {
-        if (1 != Uart4Read(&pkt.cmd_len, 1)) return -1;
-        if (1 != Uart4Read(&pkt.cmd, 1)) return -1;
+        if (1 != Uart4Read((char *)&pkt.cmd_len, 1)) return -1;
+        if (1 != Uart4Read((char *)&pkt.cmd, 1)) return -1;
 
         len = MyMini(pkt.cmd_len - 2, TDMX_UART_CMD_VALUE_MAX);
         for (i = 0; i < len; )
@@ -117,9 +133,9 @@ int TDMxRead(u8 *pCmdVal2Read, u8 *pCmdValLen)
             if (1 != Uart4Read(pStart++, 1)) return -1;
         }
 
-        if (1 != Uart4Read(&tmp, 1)) return -1;
+        if (1 != Uart4Read((char *)&tmp, 1)) return -1;
         checksum = (tmp << 8);
-        if (1 != Uart4Read(&tmp, 1)) return -1;
+        if (1 != Uart4Read((char *)&tmp, 1)) return -1;
         checksum += tmp;
 
         if (checksum == calcPktChecksum(&pkt))
@@ -127,6 +143,53 @@ int TDMxRead(u8 *pCmdVal2Read, u8 *pCmdValLen)
             //Pkt OK
             *pCmdValLen = pkt.cmd_len - 2;
             memcpy(pCmdVal2Read, pkt.cmd_val_checksum, *pCmdValLen);
+            return *pCmdValLen;
+        }
+    }
+    else
+    {
+        return -2;
+    }
+    return -3; //checksum error
+}
+
+/*****************************************************************************
+ Prototype    : waitForTDMxAck
+ Description  : wait for TDMx Ack
+ Input        : void  
+ Output       : None
+ Return Value : 0 : sucess, <0 : fail
+ Calls        : 
+ Called By    : 
+ 
+  History        :
+  1.Date         : 2015/9/18
+    Author       : qiuweibo
+    Modification : Created function
+
+*****************************************************************************/
+static int waitForTDMxAck(void)
+{
+    TDMxUartPkt pkt;
+    u16 checksum = 0;
+    u8 tmp;
+
+    if (1 != Uart4Read((char *)&pkt.header1, 1)) return -1;
+    if (1 != Uart4Read((char *)&pkt.header2, 1)) return -1;
+
+    if ((TDMX_UART_PKT_HEADER1 == pkt.header1) \
+        && (TDMX_UART_PKT_HEADER2 == pkt.header2))
+    {
+        if (1 != Uart4Read((char *)&pkt.cmd_len, 1)) return -1;
+        if (1 != Uart4Read((char *)&pkt.cmd, 1)) return -1;
+        
+        if (1 != Uart4Read((char *)&tmp, 1)) return -1;
+        checksum = (tmp << 8);
+        if (1 != Uart4Read((char *)&tmp, 1)) return -1;
+        checksum += tmp;
+
+        if ((checksum == calcPktChecksum(&pkt)) && (CMD_ACK == pkt.cmd))
+        {
             return 0;
         }
     }
@@ -134,11 +197,28 @@ int TDMxRead(u8 *pCmdVal2Read, u8 *pCmdValLen)
     {
         return -2;
     }
+    return -3; //checksum error
 }
 
+/*****************************************************************************
+ Prototype    : TDMxWrite
+ Description  : write cmd to TDMx
+ Input        : const u8 cmd             
+                const u8 *pCmdVal2Write  
+                const u8 nCmdValLen      
+ Output       : None
+ Return Value : 0: success, <0:fail
+ Calls        : 
+ Called By    : 
+ 
+  History        :
+  1.Date         : 2015/9/18
+    Author       : qiuweibo
+    Modification : Created function
+
+*****************************************************************************/
 int TDMxWrite(const u8 cmd, const u8 *pCmdVal2Write, const u8 nCmdValLen)
 {
-    int i;
     TDMxUartPkt pkt;
     u8 len = MyMini(nCmdValLen, TDMX_UART_CMD_VALUE_MAX);
     u16 checksum = 0;
@@ -153,7 +233,18 @@ int TDMxWrite(const u8 cmd, const u8 *pCmdVal2Write, const u8 nCmdValLen)
 
     cleanUartRxBuffer();
     Uart4Write((char *)&pkt, pkt.cmd_len + 4);          //2bytes header + 2bytes checksum
-    return 0;
+
+    if ((CMD_CONNECT == cmd) \
+        || (CMD_UNCONNECT == cmd) \
+        || (CMD_SET_DP_SEL == cmd) \
+        || (CMD_SET_SPS == cmd) \
+        || (CMD_SET_BAUD == cmd)\
+        || (CMD_SET_RANGE == cmd))
+    {
+        return waitForTDMxAck();
+    }
+    
+    return 0; // normal return
 }
 
 int TDMxCtrl(void)
