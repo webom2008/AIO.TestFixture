82841af384c73aaaae8b9682bb22235339479f6e
webom2008 <webom2008@hotmail.com>
2015-11-20 17:34:11 +0800

    comment all debug switch.

--- MainMCU/MainApps/Embedded/utilities/sysconfigs.h
+++ MainMCU/MainApps/Embedded/utilities/sysconfigs.h
@@ -48,30 +48,30 @@ extern "C"{
 
 
 
-#define SKIP_CHECK_DPM2200_CONNECT          //屏蔽DPM2200压力计
+//#define SKIP_CHECK_DPM2200_CONNECT          //屏蔽DPM2200压力计
 //#define SKIP_CHECK_WAVEFORM_CONNECT         //屏蔽波形信号发生器
 
 //#define SKIP_BUZZER_ALARM
-#define SKIP_STATE_DETECT_D3V3E_POWER
-#define SKIP_STATE_DETECT_OTHER_POWER
-#define SKIP_STATE_DOWNLOAD_AIOSTM_BOOT
-#define SKIP_STATE_DOWNLOAD_AIODSP_APP
-#define SKIP_STATE_DOWNLOAD_AIOSTM_APP
-#define SKIP_STATE_NIBP_VERIFY
-#define SKIP_STATE_AIOBOARD_MAX_CURRENT
-#define SKIP_STATE_NIBP_GAS_CONTROL
-#define SKIP_STATE_NIBP_OVER_PRESS
-#define SKIP_STATE_ECG_SELFCHECK            //OK
-#define SKIP_STATE_ECG_AMPLITUDE_BAND       //??
-#define SKIP_STATE_ECG_PROBE_OFF            //??
-#define SKIP_STATE_ECG_POLARITY             //OK
-#define SKIP_STATE_ECG_PACE                 //??
-#define SKIP_STATE_ECG_QUICK_QRS            //OK
-#define SKIP_STATE_RESP_AMPLITUDE_BAND      //??
-#define SKIP_STATE_TEMP_SELFCHECK           //OK
-#define SKIP_STATE_TEMP_PROBE_OFF           //??
-#define SKIP_STATE_TEMP_PRECISION           //??
-#define SKIP_STATE_SPO2_UART                //OK
+//#define SKIP_STATE_DETECT_D3V3E_POWER
+//#define SKIP_STATE_DETECT_OTHER_POWER
+//#define SKIP_STATE_DOWNLOAD_AIOSTM_BOOT
+//#define SKIP_STATE_DOWNLOAD_AIODSP_APP
+//#define SKIP_STATE_DOWNLOAD_AIOSTM_APP
+//#define SKIP_STATE_NIBP_VERIFY
+//#define SKIP_STATE_AIOBOARD_MAX_CURRENT
+//#define SKIP_STATE_NIBP_GAS_CONTROL
+//#define SKIP_STATE_NIBP_OVER_PRESS
+//#define SKIP_STATE_ECG_SELFCHECK            //OK
+//#define SKIP_STATE_ECG_AMPLITUDE_BAND       //??
+//#define SKIP_STATE_ECG_PROBE_OFF            //??
+//#define SKIP_STATE_ECG_POLARITY             //OK
+//#define SKIP_STATE_ECG_PACE                 //??
+//#define SKIP_STATE_ECG_QUICK_QRS            //OK
+//#define SKIP_STATE_RESP_AMPLITUDE_BAND      //??
+//#define SKIP_STATE_TEMP_SELFCHECK           //OK
+//#define SKIP_STATE_TEMP_PROBE_OFF           //??
+//#define SKIP_STATE_TEMP_PRECISION           //??
+//#define SKIP_STATE_SPO2_UART                //OK
 
 #ifdef __cplusplus
 #if __cplusplus
